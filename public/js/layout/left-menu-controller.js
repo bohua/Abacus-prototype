e8d77cbae1cefd180867303a62b58cc4c637bbd8
bohua <fantasybohua@gmail.com>
2014-02-10 16:03:31 +0100

    build up skeleton

--- public/js/layout/left-menu-controller.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Bli on 14-2-10.
- */
-bleach.controller('leftMenuController', ['$scope', '$http', function ($scope, $http) {
-	$http.get('/getLeftMenu')
-		.success(function(data){
-			$scope.menu = data;
-		});
-}]);

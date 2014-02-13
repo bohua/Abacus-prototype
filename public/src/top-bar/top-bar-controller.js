13790af92d70984c26a952a36e3843464232aa97
bohua <fantasybohua@gmail.com>
2014-02-13 22:00:37 +0100

    initial skeleton

---
+++ public/src/top-bar/top-bar-controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Bohua on 14-2-13.
+ */
+
+bleach.controller('topBarController', ['$scope', function ($scope) {
+	$('#user-dropdown-menu').dropdown({
+		effect: 'fade'
+	});
+	$scope.showUserMenu = function(){
+
+	};
+}]);
